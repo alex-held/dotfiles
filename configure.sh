1f79a928a3f271d94d200197d51e3e16a056452b
Alexander Held <git@alexanderheld.net>
2019-09-07 22:29:51 +0200

    move prompt inside here

--- configure.sh
+++ configure.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 set -eou pipefail
 
-source ./script/prompt
+# script/prompt
+info () {
+  printf "\r  [ \033[00;34m..\033[0m ] $1\n"
+}
+
+user () {
+  printf "\r  [ \033[0;33m??\033[0m ] $1\n"
+}
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+fail () {
+  printf "\r\033[2K  [\033[0;31mFAIL\033[0m] $1\n"
+  echo ''
+  exit
+}
+#source ./script/prompt
 
 brewInstall () {
     # Install brew
