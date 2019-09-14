0d4bb39d4d493604f0d3db930689f888b95a0e10
alex-held <git@alexanderheld.net>
2019-09-14 03:40:46 +0200

    filter

--- configure.sh
+++ configure.sh
@@ -80,7 +80,7 @@ firstInstall () {
 
 installTaps () {  
     info 'Setting up Taps'
-    tapfile=$DOTBREW/Taps
+    tapfile="$DOTBREW/Taps"
     while read in; do brew tap "$in"; done < $(cat $tapfile | grep -v "#")
     success 'Tapped into all 3rd party taps!'
 }
